ff4a33455fa2d605d9bdc29a09224160bcd111bd
sncooneydavis <portland.pickle.chip@gmail.com>
2024-05-27 11:37:49 -0400

    updated rps with ui and changed rules (play to score of five instead of five rounds)

--- rps.js
+++ rps.js
@@ -1,96 +1,90 @@
-function playGame() {
-    let round = 1; 
-    let humanScore = 0; 
-    let computerScore = 0;
-    while (round < 6) {
-        console.log(`Round: ${round}`);
-        let computerChoice = Math.floor(Math.random() * (4 - 1) + 1);
-        let humanChoice = getHumanChoice();
-        let scoreUpdate = roundWinner(computerChoice, computerScore, humanChoice, humanScore);
-        computerScore = scoreUpdate[0];
-        humanScore = scoreUpdate[1];
-        console.log(`Your score: ${humanScore}\nComputer score: ${computerScore}`);
-        round++;
-    }
-    console.log("~GAME OVER~");
-    gameWinner(humanScore, computerScore);   
-    return;
-}
+const humanScoreE = document.querySelector("#human-score");
+const computerScoreE = document.querySelector("#computer-score");
+const choiceButtonsE = document.querySelector("#choice-menu");
+const dialogueE = document.querySelector("#dialogue");
 
-function getHumanChoice() {
-    while (true) {
-        stringChoice = prompt("What is your choice?:");
-        if (stringChoice == "rock") {
-            console.log("You play ROCK.");
-            return 1;
-        }
-        else if (stringChoice == "paper") {
-            console.log("You play PAPER.");
-            return 2;
-        }
-        else if (stringChoice == "scissors") {
-            console.log("You play SCISSORS.");
-            return 3;
-        }
-    }
-}
+let humanScore = 0; 
+let computerScore = 0;
 
-function roundWinner(computerChoice, computerScore, humanChoice, humanScore) {
-    switch(computerChoice) {
-        case 1:
-            console.log("Computer plays ROCK.");
-            switch(humanChoice) {
-                case 3:
-                    console.log("You lose this round.");
-                    return [++computerScore, humanScore];
-                case 2:
-                    console.log("You win this round!");
-                    return [++computerScore, humanScore];
+// human clicks choice
+choiceButtonsE.addEventListener("click", (event) => {
+    let computerChoice = getComputerChoice();
+    let humanChoice = event.target;
+    switch(humanChoice.id) {
+        case 'rock':
+            switch(computerChoice) {
                 case 1:
-                    console.log("You are tied.");
-                    return [computerScore, humanScore];
+                    tiedRound();
+                    break;
+                case 2:
+                    computerWinsRound();
+                    break;
+                case 3:
+                    humanWinsRound();
+                    break;
             }
             break;
-        case 2:
-            console.log("Computer plays PAPER.");
-            switch(humanChoice) {
+        case 'paper':
+            switch(computerChoice) {
                 case 1:
-                    console.log("You lose this round.");
-                    scoreUpdate = [++computerScore, humanScore];
-                    return scoreUpdate;
-                case 3:
-                    console.log("You win this round!");
-                    return [computerScore, ++humanScore];
+                    humanWinsRound();
+                    break;
                 case 2:
-                    console.log("You are tied.");
-                    return [computerScore, humanScore];
+                    tiedRound();
+                    break;
+                case 3:
+                    computerWinsRound();
+                    break;
             }
             break;
-        case 3:
-            console.log("Computer plays SCISSORS.");
-            switch(humanChoice) {
+        case 'scissors':
+            switch(computerChoice) {
                 case 1:
-                    console.log("You win this round.");
-                    return [computerScore, ++humanScore];
+                    computerWinsRound();
+                    break;
                 case 2:
-                    console.log("You lose this round!");
-                    return [++computerScore, humanScore];
+                    humanWinsRound();
+                    break;
                 case 3:
-                    console.log("You are tied.");
-                    return [computerScore, humanScore];
+                    tiedRound();
+                    break;
             }
+            break;
     }
+    updateScore(humanScoreE, humanScore, computerScoreE, computerScore);
+    if (computerScore>4) {
+        dialogueE.innerText = "Computer wins the game!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if (humanScore>4) {
+        dialogueE.innerText = "Human wins the game!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+})
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * (4 - 1) + 1);
 }
 
-function gameWinner (humanScore, computerScore) {
-    if (humanScore > computerScore) {
-        console.log("You Win the Game!!!")
-    }
-    else if (computerScore > humanScore) {
-        console.log("You lose the game...")
-    }
-    else console.log("The game is tied.")
-    return;
+function humanWinsRound() {
+    humanScore++;
+    dialogueE.innerText = "You win this round! Make your next move.";
+}
+
+function tiedRound() {
+    dialogueE.innerText = "No one wins this round. Make your next move.";
+}
+
+function computerWinsRound() {
+    computerScore++;
+    dialogueE.innerText = "The computer wins this round! Make your next move.";
+}
+
+function updateScore(humanScoreE, humanScore, computerScoreE, computerScore) {
+    humanScoreE.innerText = humanScore;
+    computerScoreE.innerText = computerScore;
 }
 
 
